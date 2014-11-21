ff6e2b05acb79b6e9c8515a8b0805bd8fc1f4739
Eric Benoit <benwa02@gmail.com>
2014-11-20 21:50:33 -0500

    Added comments table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108220540) do
+ActiveRecord::Schema.define(version: 20141121024606) do
+
+  create_table "comments", force: true do |t|
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ponds", force: true do |t|
     t.datetime "created_at"

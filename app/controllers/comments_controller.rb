063f8f5d395f076de0a6fed59b3c9a85efcdb5fb
Eric Benoit <benwa02@gmail.com>
2014-12-02 19:47:02 -0500

    Add comment refreshes to show. Added comment posted date.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 	def create
 		@user = user_session
 		@comment = Comment.create!(params.require(:comment).permit(:comment, :user_id, :pond_id))
-		redirect_to @pond
+		redirect_to :back
 	end
 
 end

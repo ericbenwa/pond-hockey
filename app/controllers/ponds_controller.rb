b553cfa1f560c8a06ee6850dd4f6e967b567fbae
Eric Benoit <benwa02@gmail.com>
2014-12-04 19:11:48 -0500

    Fixed put to puts.

--- app/controllers/ponds_controller.rb
+++ app/controllers/ponds_controller.rb
@@ -10,15 +10,15 @@ class PondsController < ApplicationController
 	# Get an individual pond.
 	def show
 		@pond = Pond.find(params[:id])
-		put @pond
+		puts @pond
 		latitude = @pond.latitude
-		put latitude
+		puts latitude
 		longitude = @pond.longitude
-		put longitude
+		puts longitude
 
 		# Get weather for pond.
 		@forecast = ForecastIO.forecast(latitude, longitude)
-		put @forecast
+		puts @forecast
 		temp = @forecast.currently.temperature.round
 
 		# Merely a demonstration of utilizing data to regurgitate.
